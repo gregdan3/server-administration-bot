65ae2b1ab8b5d0d683ff99a2a0132f92685e3c81
gregdan3 <gregory.danielson3@gmail.com>
2019-10-30 21:45:13 -0500

    Add stand-in Hello for bot; allow bot to send

--- sysadmin_telebot/main.py
+++ sysadmin_telebot/main.py
@@ -11,3 +11,12 @@ from telegram.ext import (
     Filters,
     InlineQueryHandler,
 )
+
+
+def hello(update, context):
+    update.message.reply_text("Hello {}".format(update.message.from_user.first_name))
+
+
+def send(bot, message, chat_id):
+    print(bot, message, chat_id)
+    bot.sendMessage(chat_id=chat_id, text=message)
