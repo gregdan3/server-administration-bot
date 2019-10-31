e732fff257541d01f13884a76d086bc6e6845dd0
gregdan3 <gregory.danielson3@gmail.com>
2019-10-30 21:50:53 -0500

    Test subprocess Popen

--- sysadmin_telebot/process_utils.py
+++ sysadmin_telebot/process_utils.py
@@ -8,3 +8,10 @@ import traceback
 def call_command(*args, **kwargs):
     print(args, kwargs)
     return subprocess.call(*args)
+
+
+def execute_command(*args, **kwargs):
+    proc = subprocess.Popen(
+        args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    return proc.communicate()
