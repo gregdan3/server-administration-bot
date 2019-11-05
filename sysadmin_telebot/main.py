be7c41c7284e23370abdeeb4cd9bbaf8550c67bb
gregdan3 <gregory.danielson3@gmail.com>
2019-11-05 16:52:58 -0600

    Add init_logger, wrapping command for clean logger setup

--- sysadmin_telebot/main.py
+++ sysadmin_telebot/main.py
@@ -17,6 +17,20 @@ from sysadmin_telebot.file_utils import load_token, load_yml_file
 
 __all__ = []
 
+_log = logging.getLogger(__name__)
+
+
+def init_logger(log_level, log_file, log_file_level):
+    LOG_FORMAT = (
+        "[%(asctime)s] [%(filename)22s:%(lineno)-4s] [%(levelname)8s]   %(message)s"
+    )
+    logging.basicConfig(level=log_level, format=LOG_FORMAT)
+    if log_file:
+        file_hander = logging.FileHandler(log_file)
+        file_hander.setLevel(log_file_level)
+        file_hander.setFormatter(logging.Formatter(LOG_FORMAT))
+        logging.getLogger().addHandler(file_hander)
+
 
 def bot_command(update, context, execute):
     stdout = execute()
