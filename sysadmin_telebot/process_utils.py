d50a14321ba5264f15090f869b475b9e186f1b1c
gregdan3 <gregory.danielson3@gmail.com>
2019-11-05 18:09:49 -0600

    Document known problems and explanations for strange behavior

--- sysadmin_telebot/process_utils.py
+++ sysadmin_telebot/process_utils.py
@@ -12,6 +12,12 @@ _log = logging.getLogger(__name__)
 
 
 def execute_command(*args, **kwargs):
+    """ shell=True in subprocess.Popen is considered unsafe
+        it is up to the user to provide sensible commands to execute.
+
+        KNOWN PROBLEM: 
+        A command which never outputs/sends an end of file
+        will cause proc.communicate() to hang indefinitely. """
     proc = subprocess.Popen(
         args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
@@ -19,6 +25,10 @@ def execute_command(*args, **kwargs):
 
 
 def clean_output(stdout, stderr, handler=None):
+    """ no reason to do unnecessary decodes if we do not use the output 
+        would be cleaner looking to do both decodes at top though
+
+        handler should be a function that operates on stderr str """
     if stderr:
         if handler is not None:
             stderr = handler(stderr.decode("UTF-8"))
@@ -34,6 +44,9 @@ def get_command_out(*args, **kwargs):
 
 
 def every(delay, *args, **kwargs):
+    """ args is list of Callable
+        kwargs is arguments to every Callable
+        kwargs is redundant to use of functools.partial """
     next_time = time.time() + delay
     while True:
         time.sleep(max(0, next_time - time.time()))
@@ -42,6 +55,8 @@ def every(delay, *args, **kwargs):
                 arg(**kwargs)
         except Exception:
             traceback.print_exc()
+            # intentionally eat exception
+            # bot will not fail, error does not propogate
         # skip tasks if we are behind schedule:
         next_time += (time.time() - next_time) // delay * delay + delay
 
