3a4938e212a4bdb43d201254c3712689eeb73c47
Gregory Danielson <gregory.danielson3@gmail.com>
2022-01-02 17:17:41 -0600

    Clean unnecessary parts of log_utils and make non-executable

--- sysadmin_telebot/log_utils.py
+++ sysadmin_telebot/log_utils.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env -S python3 -OO
 import logging
 
 __all__ = ["init_logger"]
-
 _log = logging.getLogger(__name__)
-
 LOG_FORMAT = (
     "[%(asctime)s] [%(filename)22s:%(lineno)-4s] [%(levelname)8s]   %(message)s"
 )
@@ -17,11 +14,3 @@ def init_logger(log_level, log_file, log_file_level):
         file_hander.setLevel(log_file_level)
         file_hander.setFormatter(logging.Formatter(LOG_FORMAT))
         logging.getLogger().addHandler(file_hander)
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
